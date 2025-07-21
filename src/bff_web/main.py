24c35a2aab71d8ae6a073f7256dc468e64ce1495
Luis Fernando Santa Ospina <l.santa@uniandes.edu.co>
2025-07-21 19:10:28 +0000

    update bff lifespan and requirements

--- src/bff_web/main.py
+++ src/bff_web/main.py
@@ -1,21 +1,12 @@
 from fastapi import FastAPI, Request
 import asyncio
-import time
-import traceback
-import uvicorn
-import uuid
-import datetime
+from contextlib import asynccontextmanager
 
-
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 from typing import Any
 
 from .consumidores import suscribirse_a_topico
-from .despachadores import Despachador
-
-from . import utils
 from .api.v1.router import router as v1
-
 from sse_starlette.sse import EventSourceResponse
 
 class Config(BaseSettings):
@@ -23,23 +14,20 @@ class Config(BaseSettings):
 
 settings = Config()
 app_configs: dict[str, Any] = {"title": "BFF-Web AeroAlpes"}
-
-app = FastAPI(**app_configs)
-tasks = list()
+tasks = []
 eventos = list()
 
-@app.on_event("startup")
-async def app_startup():
-    global tasks
-    global eventos
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     task1 = asyncio.ensure_future(suscribirse_a_topico("eventos-reserva", "aeroalpes-bff", "public/default/eventos-reserva", eventos=eventos))
     tasks.append(task1)
 
-@app.on_event("shutdown")
-def shutdown_event():
-    global tasks
+    yield
+
     for task in tasks:
         task.cancel()
+
+app = FastAPI(lifespan=lifespan, **app_configs)
 
 @app.get('/stream')
 async def stream_mensajes(request: Request):
