6447a66e769ce10df4201bc3151a762aff88e4ae
Luis Fernando Santa Ospina <l.santa@uniandes.edu.co>
2025-07-21 18:15:26 +0000

    replace gitpod by github codespaces

--- src/ui/static/js/consumidor.js
+++ src/ui/static/js/consumidor.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const messages = document.getElementById("mensajes");
 
   if(typeof(EventSource) !== "undefined") {
-    var source = new EventSource("https://8003-misw4406-tutorial9bff-cgv6a3r2t9h.ws-eu86.gitpod.io/stream");
+    var source = new EventSource("https://8003-misw4406-tutorial9bff-cgv6a3r2t9h.ws-eu86.app.github.dev/stream");
     source.onmessage = function(event) {
       const message = document.createElement("li");
       const content = document.createTextNode(event.data);
